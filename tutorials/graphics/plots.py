ff3ccf214c3db20e356ab23e4ddb76c54c581c19
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2011-08-15 12:39:26 +0100

    Update to tutorials

--- tutorials/graphics/plots.py
+++ tutorials/graphics/plots.py
@@ -6,37 +6,19 @@ import matplotlib.pyplot as plt
 import matplotlib.collections as collections
 
 # Initiate the x range and the different functions
-x = np.arange(0,8,.01)
-y1 = x*2 +1
-y2 = np.sin(x)
-y3 = np.sqrt(x)
-y4 = np.e**(-x**2)
+x = np.linspace(-4*np.pi,4*np.pi,250)
+y1 = np.sin(x)/x
+y2 = y1**2
 
 # Plot the 1st graph
-fig = plt.figure()
+fig = plt.figure(frameon=False,figsize=(6,4.5))
+fig.figurePatch.set_alpha(0.0)
 ax = fig.add_subplot(111)
+ax.autoscale(tight=True)
 ax.plot(x,y1)
-plt.savefig('figs/plot1.eps')
-
-# Plot the 2nd graph
-fig = plt.figure()
-ax = fig.add_subplot(111)
 ax.plot(x,y2)
-plt.savefig('figs/plot2.eps')
+plt.savefig('figs/plot1.eps', bbox_inches='tight')
+#plt.savefig('figs/plot1.pdf', bbox_inches='tight')
 
-# Plot the 3rd graph
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x,y2)
-ax.plot(x,y3)
-plt.savefig('figs/plot3.eps')
-
-# Plot the 4th graph
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x,y2)
-ax.plot(x,y3)
-ax.plot(x,y4)
-plt.savefig('figs/plot4.eps')
 
 # vim: tw=80
