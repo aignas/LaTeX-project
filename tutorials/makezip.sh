9da19648242b0dee5a4f7e283a044af893bed2ab
Ignas Anikevicius (gns_ank) <anikevicius@gmail.com>
2011-08-25 19:24:47 +0100

    Major update to structure and the shell script

--- tutorials/makezip.sh
+++ tutorials/makezip.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+TMP_FNAME="template_preamble.tex"
+
 function zip_folders {
-    for dir in ./*; do
+    for dir in *; do
         if [ -d ${dir} ]; then
+            echo "Making a backup dir for ${dir}"
+            cp -a ${dir} ${dir}-backup || return 0
+            cp template_preamble.tex ${dir}
+            cd ${dir}
+            texname=${dir:3}
+            sed -i "s/input{.\/\.\.\/${TMP_FNAME}}/input{\.\/${TMP_FNAME}}/" ${texname}.tex
+            echo "Deleting unnecessary files"
+            rm `ls -1 | egrep 'log|aux|out|toc|tdo|bbl|blg|nav|snm'`
+            cd ..
             echo "Generating ${dir}.zip"
             zip -q -r ${dir} ${dir}
+            rm -r ${dir}
+            mv ${dir}-backup ${dir}
         fi
     done
     return 0
@@ -20,7 +33,7 @@ function clean_zips {
 
 function upload_zips {
     for file in ./*.zip; do
-        echo "Uploading ${file}
+        echo "Uploading ${file}"
     done
     return 0
 }
